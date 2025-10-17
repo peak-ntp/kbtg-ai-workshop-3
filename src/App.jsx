3c5f47e878c1b513dd19d31642788292ff2f87ef
Nattapat Tatiyarat <nattapat.tat@kbtg.tech>
2025-10-17 11:49:52 +0700

    convert Payment.vue to react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,11 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
+import PaymentForm from "./components/PaymentForm"
 import "./App.css"
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
-        <>
-            <div className="flex justify-center gap-8 mb-8">
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1 className="text-5xl font-bold mb-8">Vite + React + Tailwind</h1>
-            <div className="card">
-                <button
-                    onClick={() => setCount((count) => count + 1)}
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                >
-                    count is {count}
-                </button>
-                <p className="mt-4">
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs text-gray-500">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <div className="min-h-screen bg-gray-900 py-12">
+            <PaymentForm />
+        </div>
     )
 }
 
